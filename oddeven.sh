524a4653c8c2ba947eb0e4e6248ebb488541a485
joicemariya38 <joicejeffi@gmail.com>
2026-02-23 14:00:47 +0530

    Add files via upload

---
+++ oddeven.sh
@@ -0,0 +1,11 @@
+check_odd_even() {
+    if [ $((number % 2)) -eq 0 ]; then
+        echo "$number is even"
+    else
+        echo "$number is odd"
+    fi
+}
+
+echo "Please enter a number:"
+read number
+check_odd_even
